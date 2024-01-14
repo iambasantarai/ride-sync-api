801d7bc4549b0782f11f47c984ae3a9711efde39
iambasantarai <iambasantarai@gmail.com>
2024-01-14 11:49:55 +0545

    Fix add friend to room feature

--- src/routes/room.routes.ts
+++ src/routes/room.routes.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 router.get('/', authMiddleware, RoomController.rooms);
 router.post('/', authMiddleware, RoomController.createRoom);
-router.post('/:roomId/add', authMiddleware, RoomController.addToRoom);
+router.post('/:roomId/add/:friendId', authMiddleware, RoomController.addToRoom);
 router.post('/:roomId/leave', authMiddleware, RoomController.leaveRoom);
 router.delete('/:roomId', authMiddleware, RoomController.deleteRoom);
 export default router;
